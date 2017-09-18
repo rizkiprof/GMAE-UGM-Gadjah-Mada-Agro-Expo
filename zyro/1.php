bac8a98fd8f61c0031a7e37e01e55ba439564048
Muhammad Rizki Ramadhana <m.rizkiramadhana@yahoo.co.id>
2017-09-18 07:03:37 +0700

    Add files via upload

---
+++ zyro/1.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Home</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.48" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1492961917" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1492961917" rel="stylesheet" type="text/css" />
+	
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance0" class="wb_element wb-menu"><ul class="hmenu"></ul><div class="clearfix"></div></div><div id="wb_element_instance1" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance2" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1">Round sunglasses</h1></div><div id="wb_element_instance3" class="wb_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">Website kami akan datang secepatnya</h5></div><div id="wb_element_instance4" class="wb_element" style=" line-height: normal;"><h3 class="wb-stl-heading3"><span style="color:#ffffff;">Teks ini harus diganti dengan informasi mengenai Anda dan bisnis Anda Teks ini harus diganti dengan informasi mengenai Anda dan bisnis Anda Teks ini harus...</span></h3></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance5" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance6" class="wb_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">65</h5></div><div id="wb_element_instance7" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance8" class="wb_element" style=" line-height: normal;"><h2 class="wb-stl-heading2" style="text-align: center;">Hari-hari</h2></div><div id="wb_element_instance9" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance10" class="wb_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">9</h5></div><div id="wb_element_instance11" class="wb_element" style=" line-height: normal;"><h2 class="wb-stl-heading2" style="text-align: center;">Jam</h2></div><div id="wb_element_instance12" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance13" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance14" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance15" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance19" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(1);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance19");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance19").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 216px;">
+	
+<div id="wb_element_instance16" class="wb_element" style=" line-height: normal;"><h2 class="wb-stl-heading2">Kontak</h2></div><div id="wb_element_instance17" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><span class="wb-stl-highlight"><strong>Alamat</strong> </span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">JAC Indonesia PT</span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">No. 37 Rt 004/06</span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">Kayuringin Jaya</span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">Jawa Barat 17144</span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">Indonesia</span></p></div><div id="wb_element_instance18" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><strong><span class="wb-stl-highlight">Phone:</span></strong></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight"><span style='color: rgb(255, 255, 255); font-family: "Trebuchet MS", sans-serif; font-size: 16px; background-color: transparent;'>+62 21 88860770</span></span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">E-mail: </span></p>
+<p class="wb-stl-normal"><span class="wb-stl-highlight">info@site.info</span></p></div><div id="wb_element_instance20" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.height(160);
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div></body>
+</html>
---
+++ zyro/news.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>News</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="News" />
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.48" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1492961917" rel="stylesheet" type="text/css" />
+	<link href="css/news.css?ts=1492961917" rel="stylesheet" type="text/css" />
+	
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance21" class="wb_element wb-menu"><ul class="hmenu"></ul><div class="clearfix"></div></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance22" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(news);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance22");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance22").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 80px;">
+	
+<div id="wb_element_instance23" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.height(0);
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div></body>
+</html>
